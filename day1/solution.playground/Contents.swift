06b61ec29bea59d823a421d3f877ae53f624bdef
Evan Cooper <evan.cooper@rogers.com>
2022-12-02 15:57:40 -0500

    day 1

---
+++ day1/solution.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+let file = Bundle.main.path(forResource: "input", ofType: "txt")
+let input = try String(contentsOfFile: file!)
+let lines = input.components(separatedBy: .newlines)
+
+var current = 0
+var elves = [Int]()
+
+lines.forEach { line in
+    guard !line.isEmpty else {
+        elves.append(current)
+        current = 0
+        return
+    }
+    guard let calories = Int(line) else { return }
+    current += calories
+}
+
+elves.sort()
+
+let range = (elves.count - 3)..<elves.count
+elves[range].reduce(0, +)
